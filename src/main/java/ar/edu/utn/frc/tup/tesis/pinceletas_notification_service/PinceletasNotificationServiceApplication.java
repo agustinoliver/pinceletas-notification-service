f484078ca23da7a2aae072a4183bdda0c39bc041
Agustín Ivan, Olivera <114453@tecnicatura.frc.utn.edu.ar>
2025-10-09 16:15:56 -0300

    primer commit

---
+++ src/main/java/ar/edu/utn/frc/tup/tesis/pinceletas_notification_service/PinceletasNotificationServiceApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.utn.frc.tup.tesis.pinceletas_notification_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PinceletasNotificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PinceletasNotificationServiceApplication.class, args);
+	}
+
+}
